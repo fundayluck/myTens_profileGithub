4e937b83f4ebecb44c1c57f8427f5cb6eac97098
jember08 <afandayul@gmail.com>
2022-03-13 01:55:57 +0700

    adding feature search bar and trying to consume API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Card from "./components/Card";
 import Desc from "./components/Desc";
-import Form from "./components/Form";
+// import Form from "./components/Form";
+import Userinput from "./components/Userinput";
 import Subtitle from "./components/Subtitle";
 import Title from "./components/Title";
 
 function App() {
+  const URL = "https://api.github.com/users";
   const [state, setState] = useState({
     imageUrl:
       "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973461_960_720.png",
@@ -14,9 +16,41 @@ function App() {
     description: "your description",
   });
 
+  const [searchText, setSearchText] = useState("");
+
+  console.log(searchText);
+
+  const handleAPI = () => {
+    fetch(`${URL}/${searchText}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setState({
+          imageUrl: result.avatar_url,
+          name: result.name,
+          username: result.login,
+          description: result.bio,
+        });
+      });
+  };
+
   return (
-    <div className="flex justify-center items-center h-[100vh]">
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+    <div className="flex justify-center items-center min-h-screen w-full">
+      <div className="">
+        {/* <div className="grid grid-cols-1 gap-12 sm:grid-cols-2"> */}
+        <div className="mb-[10px]">
+          <Userinput
+            placeholder="Search Github Username"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button
+            onClick={handleAPI}
+            type="submit"
+            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-[5px]"
+          >
+            search
+          </button>
+        </div>
         <div className="bg-[#222222] w-[350px] h-auto p-[8px] rounded shadow-[2px_4px_8px_1px_rgba(6,6,6)]">
           <div className="flex justify-center pt-[10px] pr-[0px]">
             <img
@@ -36,9 +70,9 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="w-full place-self-center">
-          <Form values={state} setValue={setState} />
-        </div>
+        {/* <div className="w-full place-self-center">
+          <Form values={state} setValues={setState} />
+        </div> */}
       </div>
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,29 @@
 import React from "react";
 import Userinput from "./Userinput";
 
-function Form({ value, setValue }) {
+function Form({ values, setValues }) {
+  const handleState = (key, value) => {
+    setValues({ ...values, [key]: value });
+  };
+
   return (
     <form>
-      <Userinput label="Image URL" />
-      <Userinput label="Name" />
-      <Userinput label="Username" />
-      <Userinput label="Description" />
+      <Userinput
+        label="Image URL"
+        onChange={(e) => handleState("imageUrl", e.target.value)}
+      />
+      <Userinput
+        label="Name"
+        onChange={(e) => handleState("name", e.target.value)}
+      />
+      <Userinput
+        label="Username"
+        onChange={(e) => handleState("username", e.target.value)}
+      />
+      <Userinput
+        label="Description"
+        onChange={(e) => handleState("description", e.target.value)}
+      />
     </form>
   );
 }
